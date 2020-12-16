b734c557c4caa7b73650074bb4061a8637d4f889
dont3t <jorcion@gmail.com>
2020-12-16 10:16:14 +0800

    deploy

---
+++ precache-manifest.364643945051371311c77181fab38259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f2366178c207c7d909",
+    "url": "/morney-website/css/app.5dbd78c1.css"
+  },
+  {
+    "revision": "7246630a2356a26465cd4fa6185631e9",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "29f2366178c207c7d909",
+    "url": "/morney-website/js/app.12d564e2.js"
+  },
+  {
+    "revision": "278ed02e057bb6344646",
+    "url": "/morney-website/js/chunk-vendors.bc356825.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
